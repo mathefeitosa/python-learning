83ee3611447b869d5dcb6371177375b2d5a366e5
Feitosa <mathe.feitosa@gmail.com>
2021-11-10 19:03:12 -0300

    small changes dice rolling.

--- dice-rolling-simulator/app.py
+++ dice-rolling-simulator/app.py
@@ -13,7 +13,7 @@ def main():
     f[number-1] = f[number-1]+1
     print(f"""
           Number: {number}
-          Math chances: {(1/s):.2f}%
+          Math chances: {(1/s):.4f}%
           Times rolled: {i}
           1: {c_p(f[0], i)} 4: {c_p(f[3], i)}
           2: {c_p(f[1], i)} 5: {c_p(f[4], i)}
@@ -27,7 +27,7 @@ def c_p(number, iterations):  # calc percentage and fix zero division
   if number == 0:
     return '0%'
   else:
-    return f'{(number*100/iterations):.2f}%'
+    return f'{(number*100/iterations):.4f}%'
 
 
 if __name__ == '__main__':
