ec4613f009f071a97059d9fd4a024bacdd9a0d54
Feitosa <mathe.feitosa@gmail.com>
2021-11-12 18:33:23 -0300

    before start a new api.

--- learning-flask/web/app.py
+++ learning-flask/web/app.py
@@ -1,12 +1,47 @@
-from operator import pos
+"""
+-> Registration of users
+-> Each user gets 10 tokens
+-> Store a sentence on our database for 1 token
+-> Retrieve his stored sentence for 1 token
+-> Password and username storage
+"""
+
+
+"""from operator import pos
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 
-from personal_utils import error
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
-
 api = Api(app)
+
+# the 'db'is the same of docker-composer
+client = MongoClient("mongodb://db:27017")
+db = client['UserNum']
+visit_counter = db['visit_counter']
+
+visit_counter.insert_one({
+    'num_of_users': 0
+})
+
+
+class Visit(Resource):
+  def get(self):
+    previous_num = visit_counter.find()[0]['num_of_users']
+    new_num = int(previous_num)+1
+    visit_counter.update({}, {'$set': {'num_of_users': new_num}})
+    return 'Hello user ' + str(new_num)
+# -------------------------------------------------------------------------------
+# PERSONAL UTILS
+
+
+def error(message):
+  return jsonify({
+      "error": message
+  })
+# -------------------------------------------------------------------------------
 
 
 def checkPostedData(posted_data, function_name):
@@ -114,6 +149,9 @@ api.add_resource(Add, '/add')
 api.add_resource(Subtract, '/subtract')
 api.add_resource(Divide, '/divide')
 api.add_resource(Multiply, '/multiply')
+api.add_resource(Visit, '/hello')
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host='0.0.0.0')
+"""
